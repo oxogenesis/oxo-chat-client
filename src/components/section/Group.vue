7e438b0bc39b45f2a4630814dd53b8c1f89b09d1
oxogenesis <49838330+oxogenesis@users.noreply.github.com>
2019-09-22 00:15:22 +0800

    m

---
+++ src/components/section/Group.vue
@@ -0,0 +1,23 @@
+<template>
+  <li
+    class="session-list-item"
+    :class="{ active: active }"
+    @click="$emit('switch-group-session', group_hash)">
+    <h5 class="session-name">{{ group_name }}</h5>
+    <div class="session-time">
+      {{ unread_count }}
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'Group',
+  props: {
+    active: Boolean,
+    group_hash: String,
+    group_name: String,
+    unread_count: Number
+  }
+}
+</script>
---
+++ src/components/section/GroupSection.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="session-section">
+    <ul class="session-list">
+      <group v-for="session in this.$store.state.OXO.GroupSessions" :group_hash="session.hash" :group_name="session.name" :unread_count="session.unread_count" :active="session.group_hash === currentGroupSession" @switch-group-session="switchGroupSession">
+      </group>
+    </ul>
+  </div>
+</template>
+<script>
+import Group from './Group.vue'
+import { mapActions, mapGetters } from 'vuex'
+
+export default {
+  name: 'GroupSection',
+  components: { Group },
+  data() {
+    return {}
+  },
+  created() {
+  },
+  computed: {
+    ...mapGetters({
+      currentGroupSession: 'currentGroupSession'
+    })
+  },
+  methods: {
+    ...mapActions({
+      switchGroupSession: 'SwitchGroupSession'
+    })
+  }
+}
+
+</script>
